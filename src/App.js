1ad4a636a51bb96254881f54ae49425d0cc37afa
Lorenzo <lorenzo.lettieri@lernende.bbw.ch>
2021-11-06 15:00:48 +0100

    finished basic example

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+    Box,
+    Button,
+    Container,
+    FormControl,
+    FormControlLabel,
+    FormLabel,
+    Grid,
+    Input,
+    InputLabel,
+    Radio,
+    RadioGroup
+} from "@mui/material";
+import {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [fName, setFName] = useState('');
+    const [lName, setLName] = useState('');
+    const [email, setEmail] = useState('');
+    const [gender, setGender] = useState('');
+
+    return (
+        <Container>
+            <Box display='flex' justifyContent='center' alignItems='center'>
+                <Grid
+                    container
+                    justifyContent='center'
+                    spacing={2}
+                    maxWidth={500}
+                    mt={5}
+                >
+                    <Grid item xs={6}>
+                        <FormControl fullWidth>
+                            <InputLabel>First Name</InputLabel>
+                            <Input required id='fName' onChange={e => setFName(e.target.value)}/>
+                        </FormControl>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <FormControl fullWidth>
+                            <InputLabel>Last Name</InputLabel>
+                            <Input required id='lName' onChange={e => setLName(e.target.value)}/>
+                        </FormControl>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <FormControl fullWidth>
+                            <InputLabel>E-Mail</InputLabel>
+                            <Input required id='email' onChange={e => setEmail(e.target.value)}/>
+                        </FormControl>
+                    </Grid>
+                    <Grid item xs={6} mt={5}>
+                        <FormControl>
+                            <FormLabel>Gender</FormLabel>
+                            <RadioGroup defaultValue='other' onChange={e => setGender(e.target.value)}>
+                                <FormControlLabel value='male' control={<Radio/>} label='Male'/>
+                                <FormControlLabel value='female' control={<Radio/>} label='Female'/>
+                                <FormControlLabel value='other' control={<Radio/>} label='Other'/>
+                            </RadioGroup>
+                        </FormControl>
+                    </Grid>
+                    <Grid item xs={6} alignSelf='center'>
+                        <Button variant='contained' onClick={printInfos}>Submit</Button>
+                    </Grid>
+                </Grid>
+            </Box>
+        </Container>
+    );
+
+    function printInfos() {
+        if (fName !== '' && lName !== '' && email !== '') {
+            console.log(fName, lName, email, gender);
+        } else {
+            alert('Please fill in all Required fields!')
+        }
+    }
 }
 
 export default App;
